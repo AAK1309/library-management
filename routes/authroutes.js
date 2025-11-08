4b374d2cd4a87cfff2ac6d7124684f8ae2916982
URK23CS1195 <aakritimasih@karunya.edu.in>
2025-11-08 23:58:00 +0530

    changed to correct route

--- routes/authroutes.js
+++ routes/authroutes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
+const User = require('../models/user');
 const jwt = require('jsonwebtoken');
-
 const SECRET_KEY = 'mysecretkey';
 
 // Register
